4cfb606e792964fef6dff93ecd75aa43aebab09c
alexamy <achasch@gmail.com>
2020-07-18 01:12:17 +0300

    refactor

--- image_read.rkt
+++ image_read.rkt
@@ -4,6 +4,11 @@
 
 (define image (bitmap "snapshot.png"))
 
+(define gradient-sample
+  (apply beside
+         (for/list ([step (range 1 16)])
+           (rectangle 1 1 "solid" (color (quotient 255 step) 0 0 255)))))
+
 ; algorithm
 ; 1. get all pixels with empty category
 ; 2. make random categories
@@ -39,32 +44,39 @@
    (for/sum ([get color-getters])
      (sqr (- (get c1) (get c2))))))
 
-(define gradient
-  (apply beside
-         (for/list ([step (range 1 16)])
-           (rectangle 1 1 "solid" (color (quotient 255 step) 0 0 255)))))
+
+;; assign new category for each pixel
+(define (assign-categories pixels colors)
+  (for/list ([pixel pixels])
+    (let ([pixel-color (cdr pixel)])
+      (cons
+       (argmin (λ (color) (distance color pixel-color)) colors)
+       pixel-color))))
+
+;; assign new average colors from pixels
+(define (get-colors pixels)
+  (map
+   (λ (lst) (apply avg (map cdr lst)))
+   (group-by car pixels)))
+
+;; create image from pixels with same size as provided image
+(define (image-new pixels source)
+  (let ([width  (image-width image)]
+        [height (image-height image)])
+  (color-list->bitmap pixels width height)))
 
 ;; main procedure
-(define (simplify-image2 image color-count #:precision [precision 4])
-  (void))
-
 (define (simplify-image image color-count #:precision [precision 4])
   (define color-list (image->color-list image))
   (define colors-start (build-list color-count (λ (x) (list-ref color-list (random (length color-list))))))
   (define pixels-start (map (λ (color) (cons #f color)) color-list))
+  
   (let rec ([pixels pixels-start] [colors colors-start])
-    (define pixels-new
-      (for/list ([pixel pixels])
-        (cons
-         (argmin (λ (color) (distance color (cdr pixel))) colors)
-         (cdr pixel))))
-    (define colors-new
-      (map (λ (lst) (apply avg (map cdr lst))) (group-by car pixels-new)))
+    (define pixels-new (assign-categories pixels colors))
+    (define colors-new (get-colors pixels-new))
     (define min-distance
       (apply min (map distance colors colors-new)))
+    
     (cond
-      [(<= min-distance precision)
-       (values colors
-               (color-list->bitmap (map car pixels) (image-width image) (image-height image)))]
-      [else (rec pixels-new colors-new)])))
-  
+      [(> min-distance precision) (rec pixels-new colors-new)]
+      [else (values colors (image-new (map car pixels) image))])))
