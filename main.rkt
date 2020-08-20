a95c8012e32dc806430ff60ca1427394c317db20
Alexander Chaschin <achasch@gmail.com>
2020-08-21 00:19:04 +0300

    add start wrapper in main

--- main.rkt
+++ main.rkt
@@ -4,8 +4,12 @@
   2htdp/image
   (only-in "image.rkt" pretty-simplify))
 
-(define file-name (vector-ref (current-command-line-arguments) 0))
-(define image (bitmap/file file-name))
+#;(current-command-line-arguments #("snapshot.png"))
 
-(define image-simplified (pretty-simplify image 8))
-(save-image image-simplified "image-simplified.png")
+(define (start)
+  (define file-name (vector-ref (current-command-line-arguments) 0))
+  (define image (bitmap/file file-name))
+  (define image-simplified (pretty-simplify image 8))
+  (save-image image-simplified "image-simplified.png"))
+
+(start)
